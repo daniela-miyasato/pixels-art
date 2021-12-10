d5e9bf56251cd593c26829c7491373c56ebe5926
Daniela Miyasato Goto <daniela.miyasato@live.com>
2021-12-10 20:11:17 -0300

    Requisitos 4 e 5 - cria 25 pixels e configura

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// Requisittos 2 e 3
+
 // captura elemento
-let paleta = document.getElementById('color-palette');
+let palette = document.getElementById('color-palette');
 
 // cria uma div para colocar o quadradinho com a cor
 let black = document.createElement('div');
@@ -8,14 +10,35 @@ let black = document.createElement('div');
 black.className = 'color black';
 
 // adiciona na página
-paleta.appendChild(black);
+palette.appendChild(black);
 
 // fazer o mesmo com as outras 3 cores
-function criaCor(paleta, classes) {
+function criaCor(palette, classes) {
   let cor = document.createElement('div');
   cor.className = classes;
-  paleta.appendChild(cor);
+  palette.appendChild(cor);
 }
-criaCor(paleta, 'color yellow');
-criaCor(paleta, 'color pink');
-criaCor(paleta, 'color aqua');
+criaCor(palette, 'color yellow');
+criaCor(palette, 'color pink');
+criaCor(palette, 'color aqua');
+
+// Requisito 4
+// fazer um quadro de pixels, com 25 pixels
+
+// fazer 1 pixel
+// let board = document.getElementById('pixel-board');
+// let onePixel = document.createElement('div');
+// onePixel.className = 'pixel white';
+// board.appendChild(onePixel);
+
+// adicionar 25 pixels.
+
+function criaPixels(n) {
+  let board = document.getElementById('pixel-board');
+  for (let index = 0; index < n; index += 1) {
+    let onePixel = document.createElement('div');
+    onePixel.className = 'pixel white';
+    board.appendChild(onePixel);
+  }
+}
+criaPixels(25);
